f7e993e6e9b8d2329651e61cb4ed5ca8d9c7a2fe
Ossi <ossian.beukelman@gmail.com>
2015-08-02 23:06:22 +0300

    Add CalculatorTemplet
    Added Calulator Templet

---
+++ Controls/Tools/Claculator/CalculatorControl.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Controls.Tools.Claculator
+{
+    partial class CalculatorControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CalculatorControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "CalculatorControl";
+            this.Size = new System.Drawing.Size(247, 335);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
